fc94ce9caba3ceed87367b9580248d5b3921f358
BeckmaR <rene.bckmnn@gmail.com>
2016-06-07 22:25:38 +0200

    Added server class to server project

---
+++ src/server/EMUServer/server.cpp
@@ -0,0 +1,6 @@
+#include "server.h"
+
+server::server(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ src/server/EMUServer/server.h
@@ -0,0 +1,16 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+
+class server : public QObject
+{
+    Q_OBJECT
+public:
+    explicit server(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // SERVER_H
